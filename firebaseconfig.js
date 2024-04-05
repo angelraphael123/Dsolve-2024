60b829a133878146e936c9830cd87205e767e396
harikrishnan-git <haribkrishh@gmail.com>
2024-04-05 17:19:22 +0530

    everything

---
+++ firebaseconfig.js
@@ -0,0 +1,12 @@
+// Your web app's Firebase configuration
+// For Firebase JS SDK v7.20.0 and later, measurementId is optional
+export const firebaseConfig = {
+    apiKey: "AIzaSyDyJnyzT9JOSKGVnKC_X0Yt02l8c7sjpSg",
+    authDomain: "website1-60ca0.firebaseapp.com",
+    projectId: "website1-60ca0",
+    storageBucket: "website1-60ca0.appspot.com",
+    messagingSenderId: "865726987459",
+    appId: "1:865726987459:web:2fbbe251f529ab1a8ab676",
+    measurementId: "G-1QB8N0X3PJ"
+  };
+  
